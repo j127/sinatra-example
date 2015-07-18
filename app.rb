8831d359f5887abf8bd9b28acb828b558031d1cf
J. Cohen <hostelmarketing@gmail.com>
2015-07-18 13:18:46 -0700

    Add CSS and page #2

--- app.rb
+++ app.rb
@@ -3,8 +3,15 @@ require 'sinatra'
 get '/' do
   # Do some logic and create your variables
   title = 'Saluton, Mondo!'
-  content = 'This is a webpage!'
+  content = 'This is the homepage!'
 
   # Tell it which template to use and pass the variables to it
+  erb :index, :locals => {:title => title, :content => content}
+end
+
+get '/about' do
+  title = 'About'
+  content = 'This is the about page.'
+
   erb :index, :locals => {:title => title, :content => content}
 end
